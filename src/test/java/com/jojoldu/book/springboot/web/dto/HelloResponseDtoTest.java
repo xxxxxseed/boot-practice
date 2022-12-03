af6cd6e1ff010f4a170debd6bc02432d928105cb
xxxxxseed <xxxxxseed@gmail.com>
2022-12-03 10:47:53 +0900

    git test

---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import org.assertj.core.data.Percentage;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+//        assertThat(dto.getName()).isEqualTo(name);
+//        assertThat(dto.getAmount()).isEqualTo(amount);
+        assertThat(dto.getName()).as("HelloResponseDto name")
+                .isEqualToIgnoringCase("TEST");
+        assertThat(dto.getAmount()).as("HelloResponseDto amount")
+                .isCloseTo(1050, Percentage.withPercentage(5.2));
+    }
+}
